9ad5d1c0927682f454abccf5233036d13c3bc620
maliang <295011445@qq.com>
2016-03-08 17:20:07 +0800

    ...

--- DianXinHUD/DianXinHUD/ViewController.swift
+++ DianXinHUD/DianXinHUD/ViewController.swift
@@ -12,36 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //DXHUD.showHud(inView: self.view)
-        let view = UIView.init(frame: CGRectMake(100, 100, 5, 5))
-        view.backgroundColor = UIColor.orangeColor()
-        self.view.addSubview(view)
-        let shape = CAShapeLayer()
-        let arcPath = UIBezierPath.init(arcCenter: CGPointMake(300, 200), radius: 2, startAngle: 0, endAngle: CGFloat(M_PI * 2), clockwise: true)
-        shape.path = arcPath.CGPath
-        shape.lineWidth = 4
-        shape.fillColor = UIColor.clearColor().CGColor
-        shape.strokeColor = UIColor.greenColor().CGColor
-        self.view.layer.addSublayer(shape)
-        
-        
-        let layer = CALayer()
-        layer.frame = CGRectMake(100, 100, 50, 50)
-        layer.backgroundColor = UIColor.greenColor().CGColor
-        self.view.layer.addSublayer(layer)
-        
-        let rotaionAnimation = CABasicAnimation.init(keyPath: "affineTransform")
-        rotaionAnimation.toValue = NSValue.init(CGAffineTransform:CGAffineTransformMakeRotation(CGFloat(M_PI)))
-        rotaionAnimation.duration = 3
-        rotaionAnimation.repeatCount = MAXFLOAT
-        //        rotaionAnimation.calculationMode = kCAAnimationPaced
-        //        rotaionAnimation.rotationMode = kCAAnimationRotateAuto
-        shape.addAnimation(rotaionAnimation, forKey: "rotaion")
-        
-        layer.addAnimation(rotaionAnimation, forKey: "rotaion1")
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func beginAnimation(sender: UIButton) {
+        
+        if sender.tag == 1
+        {
+            DXHUD.showHud(remindTitle: "加载中...", flag: "test", inView: self.view, confi: { (hud:DXHUD) -> () in
+            
+                hud.desLabel.textColor = UIColor.redColor()
+           })
+        }
+        else if sender.tag == 2
+        {
+           DXHUD.hiddenHud(hudFlag: "test")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
